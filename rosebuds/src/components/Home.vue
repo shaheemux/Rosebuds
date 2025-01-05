63395f972657e345123b79525a9cbd51530d7786
unknown <Shaheemsalie349@gmail.com>
2025-01-05 19:20:46 +0200

    added stuff

---
+++ rosebuds/src/components/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="home">
+        <p class="m-12">Lorem ipsum dolor sit amet </p>
+    </div>
+    <div></div>
+</template>
---
+++ rosebuds/src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+   <Navbar /> 
+   <Home /> 
+  </div>
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue"
+import Home from "../components/Home.vue"
+
+
+export default {
+  components : {
+    Navbar,
+    Home
+  }
+}
+</script>
+
+
+
+<!-- reference for importing components  -->
+
+<!-- 
+import Navbar from "../components/Navbar.vue"
+  import Home from "../components/Home.vue"
+  import About from "../components/About.vue"
+
+
+  export default {
+    components: {
+      Navbar,
+      Home,
+      About,
+    }
+  } 
+    <Navbar />
+    <Home />
+    <About />  
+  -->
